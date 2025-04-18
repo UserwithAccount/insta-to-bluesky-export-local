50444e083be17235872c430305fd6cec926c3ebb
UserwithAccount <carlzepke@proton.me>
2025-04-18 10:58:17 +0200

    fix

--- pages/api/test/processScheduled.ts
+++ pages/api/test/processScheduled.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/cron/processScheduled`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/cron/runScheduledPosts`, {
         method: "POST",
       });
       const data = await response.json();
